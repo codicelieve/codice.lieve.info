54bb2dae4f4d95f206c8062a1b5bf1e38d9df077
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2020-07-11 22:10:05 +0100

    The balloon goes up

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import {
-  Engine, Render, World, Bodies, Body, Composites, Composite, Constraint,
-  Mouse, MouseConstraint,
-
+  Bodies, Body, Composite, Composites, Constraint, Engine, Events,
+  Mouse, MouseConstraint, Render, World,
 } from '../node_modules/matter-js/build/matter.min';
+
+const SCENE_WIDTH = 800;
+const SCENE_HEIGHT = 600;
+const STRING_X = 200;
+const LINKS_NUM = 8;
+const LINKS_SEP = 10;
+const LINKS_LENGTH = 40;
+const STRING_WIDTH = 5;
+const BALLOON_SIZE = 80;
+
+const GROUND_Y = SCENE_HEIGHT - 20;
 
 // create an engine
 const engine = Engine.create();
@@ -13,33 +23,57 @@ const render = Render.create({
   engine,
 });
 
+const ground = Bodies.rectangle(
+  SCENE_WIDTH / 2, GROUND_Y + 50, SCENE_WIDTH + 10, 100, {isStatic: true},
+);
+
 const group = Body.nextGroup(true);
 
-const string = Composites.stack(100, 40, 8, 1, 10, 10,
-  (x, y) => Bodies.rectangle(x, y, 40, 5, {collisionFilter: {group}}));
-
+const string = Composites.stack(
+  STRING_X, GROUND_Y - STRING_WIDTH / 2, LINKS_NUM, 1, LINKS_SEP, 0,
+  (x, y) => Bodies.rectangle(
+    x, y, LINKS_LENGTH, STRING_WIDTH,
+    {
+      collisionFilter: {group},
+      density: 0.0001,
+    }
+  ),
+);
 Composites.chain(string, 0.5, 0, -0.5, 0,
   {stiffness: 0.8, length: 2, render: {type: 'line'}});
 
 Composite.add(string, Constraint.create({
-  bodyB: string.bodies[0],
-  pointB: {x: -25, y: 0},
-  pointA: {x: string.bodies[0].position.x, y: string.bodies[0].position.y},
+  pointA: {
+    x: string.bodies[LINKS_NUM - 1].position.x,
+    y: string.bodies[LINKS_NUM - 1].position.y,
+  },
+  bodyB: string.bodies[LINKS_NUM - 1],
+  pointB: {x: LINKS_LENGTH / 2, y: 0},
   stiffness: 0.5,
 }));
 
-const balloon = Bodies.rectangle(100 + 50 * 8 + 40, 50 + 40, 80, 80);
-Composite.add(string, balloon);
+// Attach a balloon to the string
+const balloon = Bodies.rectangle(
+  string.bodies[0].position.x - LINKS_LENGTH / 2 - BALLOON_SIZE / 2,
+  string.bodies[0].position.y - BALLOON_SIZE / 2,
+  BALLOON_SIZE, BALLOON_SIZE,
+  {density: 0.0001},
+);
 
 Composite.add(string, Constraint.create({
-  bodyA: string.bodies[7],
-  pointA: {x: 25, y: 2.5},
+  bodyA: string.bodies[0],
+  pointA: {x: -LINKS_LENGTH / 2, y: 0},
   bodyB: balloon,
-  pointB: {x: -40, y: -40},
+  pointB: {x: BALLOON_SIZE / 2, y: BALLOON_SIZE / 2},
   stiffness: 0.5,
 }));
 
-const ground = Bodies.rectangle(400, 610, 810, 60, {isStatic: true});
+Composite.add(string, balloon);
+
+// Apply a vertical force to the balloon
+Events.on(engine, 'afterUpdate', () => {
+  Body.applyForce(balloon, {x: balloon.position.x, y: -10}, {x: 0, y: -0.001});
+});
 
 // add all of the bodies to the world
 World.add(engine.world, [string, ground]);
